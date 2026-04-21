6de72a619b37aa56f98e1d54ff19e56d3dd99d40
Kádár Ambrus <you@example.com>
2026-04-21 18:50:38 +0200

    vegleges

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -28,7 +28,7 @@ export const ProductSection = () => {
 
         <h1>Fall Limited Edition Sneakers</h1>
 
-        <p>These low-profile sneakers are your perfect casual wear companion. Featuring a 
+        <p>These Low-profile sneakers are your perfect casual wear companion. Featuring a 
         durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
 
         <strong>$125.00</strong>
